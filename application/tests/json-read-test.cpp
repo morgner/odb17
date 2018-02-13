adb9092cf0ff95b6d18f5d4a9dfbe4a6e12536b2
Manfred Morgner <manfred.morgner@gmail.com>
2018-02-13 07:56:08 +0100

    minor cleanup

--- application/tests/json-read-test.cpp
+++ application/tests/json-read-test.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <fstream>
 
-//#include <json/value.h>
 #include <json/json.h>
-//#include <json/reader.h>
-
-// allocator.h   assertions.h  autolink.h    config.h      features.h    forwards.h    json.h        reader.h      value.h       version.h     writer.h
-
 
 #include "odb.h"
 #include "atom.h"
@@ -14,22 +9,6 @@
 #include "property.h"
 #include "generic.h"
 
-/*
-int main() {
-    ifstream ifs("alice.json");
-    Json::Reader reader;
-    Json::Value obj;
-    reader.parse(ifs, obj); // reader can also read strings
-    cout << "Book: " << obj["book"].asString() << endl;
-    cout << "Year: " << obj["year"].asUInt() << endl;
-    const Json::Value& characters = obj["characters"]; // array of characters
-    for (int i = 0; i < characters.size(); i++){
-        cout << "    name: " << characters[i]["name"].asString();
-        cout << " chapter: " << characters[i]["chapter"].asUInt();
-        cout << endl;
-        }
-    }
-*/
 
 odb::COdb oOdb;
 
@@ -41,25 +20,15 @@ int main()
 
     const Json::Value & properties = json["Object Database Dump"]["Properties"];
     std::cout << "odb read in " << properties.size() << " properties.\n";
-
-    const Json::Value & atoms = json["Object Database Dump"]["Atoms"];
-    std::cout << "odb read in " << atoms.size() << " atoms.\n";
-
-    const Json::Value & reasons = json["Object Database Dump"]["Reasons"];
-    std::cout << "odb read in " << reasons.size() << " reasons.\n";
-
-    const Json::Value & things = json["Object Database Dump"]["Things"];
-    std::cout << "odb read in " << things.size() << " things.\n";
-
-    // Iterate over the sequence elements.
     for ( size_t index = 0; index < properties.size(); ++index )
         {
         auto nId   = properties[(int)index].get("id",    0).asUInt();
         auto sName = properties[(int)index].get("name", "").asString();
         oOdb.MakeProperty(/*nId, */sName);
-//        std::cout << "Property ID: " << nId << ", name: " << sName << '\n';
-	}
-    // Iterate over the sequence elements.
+        }
+
+    const Json::Value & atoms = json["Object Database Dump"]["Atoms"];
+    std::cout << "odb read in " << atoms.size() << " atoms.\n";
     for ( size_t index = 0; index < atoms.size(); ++index )
         {
         auto nId     = atoms[(int)index].get("id",      0).asUInt();
@@ -69,23 +38,24 @@ int main()
         auto sFormat = atoms[(int)index].get("format", "").asString();
         auto sData   = atoms[(int)index].get("data",   "").asString();
         oOdb.MakeAtom(/*nId, */sData, sName, sPrefix, sSuffix, sFormat);
-//        std::cout << "Atom ID: " << nId << ", name: " << sName << '\n';
-	}
-    // Iterate over the sequence elements.
+        }
+
+    const Json::Value & reasons = json["Object Database Dump"]["Reasons"];
+    std::cout << "odb read in " << reasons.size() << " reasons.\n";
     for ( size_t index = 0; index < reasons.size(); ++index )
         {
         auto nId   = reasons[(int)index].get("id",    0).asUInt();
         auto sName = reasons[(int)index].get("name", "").asString();
         oOdb.MakeReason(/*nId, */sName);
-//        std::cout << "Reason ID: " << nId << ", name: " << sName << '\n';
-	}
-    // Iterate over the sequence elements.
+        }
+
+    const Json::Value & things = json["Object Database Dump"]["Things"];
+    std::cout << "odb read in " << things.size() << " things.\n";
     for ( size_t index = 0; index < things.size(); ++index )
         {
         auto nId   = things[(int)index].get("id",    0).asUInt();
         auto sName = things[(int)index].get("name", "").asString();
-
-	auto pThing = oOdb.MakeThing(/*nId, */sName);
+        oOdb.MakeThing(/*nId, */sName);
         }
     for ( size_t index = 0; index < things.size(); ++index )
         {
@@ -95,7 +65,7 @@ int main()
         for ( size_t i = 0; i < p.size(); ++i )
             {
             auto nPId = p[(int)i].get("id", 0).asUInt();
-	    oOdb.AppendProperty2Thing( nId, nPId );
+            oOdb.AppendProperty2Thing( nId, nPId );
             }
 
         const Json::Value & a = things[(int)index]["atoms"];
@@ -112,8 +82,7 @@ int main()
             auto nRId = l[(int)i].get("reason-id", 0).asUInt();
             oOdb.LinkThing2Thing( nId, nTId, nRId );
             }
-//        std::cout << "Thing ID: " << nId << ", name: " << sName << '\n';
-	}
+        }
     oOdb.print_json(std::cout);
     }
 /*
@@ -128,7 +97,7 @@ int main()
                 { "id": 0, "name": "Wundertüte",
                     "properties": [ {"id": "0"},{"id": "1"} ],
                     "atoms": [ {"id": "13"},{"id": "14"} ],
-                    "links": [ {"thing-id": "6", "reason-id": "3"} ] },
+                    "links": [ {"thing-id": "6", "reason-id": "3"} ] }
             ]
         }
 }
